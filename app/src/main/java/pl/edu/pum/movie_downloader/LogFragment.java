1b5e3f5776c389581e99c64ace8a4fcb38dde321
Mateusz Korzeniowski <mateuszkorzeniowski98@gmail.com>
2021-03-26 11:32:44 +0100

    Create register form

--- app/src/main/java/pl/edu/pum/movie_downloader/LogFragment.java
+++ app/src/main/java/pl/edu/pum/movie_downloader/LogFragment.java
@@ -1,10 +1,16 @@
 package pl.edu.pum.movie_downloader;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,8 +20,13 @@ import androidx.navigation.Navigation;
 
 public class LogFragment extends Fragment
 {
-    private Button mRegisterButton;
+    private Button mLogInButton;
     private TextView mRegisterTextView;
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private ImageButton mGoogleSignImageButton;
+    private ImageButton mFacebookSignImageButton;
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState)
     {
@@ -28,10 +39,15 @@ public class LogFragment extends Fragment
     {
         View view = inflater.inflate(R.layout.log_fragment, container, false);
 
-        mRegisterButton = view.findViewById(R.id.login_button);
+        mLogInButton = view.findViewById(R.id.login_button);
         mRegisterTextView = view.findViewById(R.id.register_text_view);
+        mEmailEditText = view.findViewById(R.id.email_field);
+        mPasswordEditText = view.findViewById(R.id.password_field);
+        mGoogleSignImageButton = view.findViewById(R.id.google_login_button);
+        mFacebookSignImageButton = view.findViewById(R.id.facebook_login_button);
 
-        mRegisterButton.setOnClickListener(new View.OnClickListener()
+
+        mLogInButton.setOnClickListener(new View.OnClickListener()
         {
             @Override
             public void onClick(View v)
@@ -49,7 +65,77 @@ public class LogFragment extends Fragment
             }
         });
 
+        mGoogleSignImageButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //TODO log by Google
+            }
+        });
 
+        mFacebookSignImageButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //Todo log by Facebook
+            }
+        });
+        AddClearButton(mEmailEditText);
+        AddClearButton(mPasswordEditText);
         return view;
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    public static void AddClearButton(EditText edt)
+    {
+        edt.addTextChangedListener(new TextWatcher()
+        {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s)
+            {
+                if (s.length() > 0)
+                {
+                    edt.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0, R.drawable.ic_baseline_clear_24,0);
+                }
+                else
+                {
+                    edt.setCompoundDrawablesRelativeWithIntrinsicBounds(0,0, 0,0);
+                }
+            }
+        });
+
+        edt.setOnTouchListener(new View.OnTouchListener()
+        {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View v, MotionEvent event)
+            {
+                if (event.getAction() == MotionEvent.ACTION_UP)
+                {
+                    if (edt.getCompoundDrawables()[2] != null)
+                    {
+                        if(event.getX() >= (edt.getRight()- edt.getLeft() - edt.getCompoundDrawables()[2].getBounds().width()))
+                        {
+                            edt.setText("");
+                        }
+                    }
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/pl/edu/pum/movie_downloader/RegisterFragment.java
+++ app/src/main/java/pl/edu/pum/movie_downloader/RegisterFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,7 +14,11 @@ import androidx.navigation.Navigation;
 
 public class RegisterFragment extends Fragment
 {
-    private Button mLogButton;
+    private EditText mNickEditView;
+    private EditText mEmailEditText;
+    private EditText mPasswordEditText;
+    private EditText mRepeatedPasswordEditText;
+    private Button mRegisterButton;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState)
     {
@@ -25,6 +30,26 @@ public class RegisterFragment extends Fragment
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
         View view = inflater.inflate(R.layout.register_fragment, container, false);
+
+        mNickEditView = view.findViewById(R.id.nick);
+        mEmailEditText = view.findViewById(R.id.email);
+        mPasswordEditText = view.findViewById(R.id.password);
+        mRepeatedPasswordEditText = view.findViewById(R.id.repeat_password);
+        mRegisterButton = view.findViewById(R.id.register_button);
+
+        mRegisterButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                //TODO add new account
+            }
+        });
+
+        LogFragment.AddClearButton(mNickEditView);
+        LogFragment.AddClearButton(mEmailEditText);
+        LogFragment.AddClearButton(mPasswordEditText);
+        LogFragment.AddClearButton(mRepeatedPasswordEditText);
         return view;
     }
 }
